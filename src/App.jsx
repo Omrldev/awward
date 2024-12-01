a0a0d9554fce242bc90a6a4fffef3273e4a97e22
Omar Ledesma <joseomar.ledagui@gmail.com>
2024-12-01 16:31:20 +0000

    implement text

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import Hero from './components/Hero'
+import Prueba from './components/Prueba'
 
 const App = () => {
 
   return (
-    <main>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Hero />
+      <Prueba />
     </main>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,48 +1,81 @@
 import React, { useRef, useState } from 'react'
 
 const Hero = () => {
-  // Index of the video that is playing
   const [currentIndex, setCurrentIndex] = useState(1);
-  // We like to know if the user has click anything
   const [hasClicked, setHasClicked] = useState(false)
-  // A video does take a long to start
   const [isLoading, setIsLoading] = useState(true)
-  // The numbers of videos that has loaded
   const [loadedVideos, setLoadedVideos] = useState(0)
 
-  const totalVideos = 4;
+  const totalVideos = 3;
 
-  // We need to add a Reference which allows us to switch between thos videos or target the player wich i 
   const nextVideoRef = useRef(null);
 
-  /*const getIndex = () => {
-    setCurrentIndex((prevIndex) => prevIndex + 1)
-  }*/
-
-  const miniVideoClick = () => {
-    // When we click in this
-    setHasClicked(true);
-
-    setCurrentIndex((prevIndex) => prevIndex +1);    
+  const handleVideoLoad = () => {
+    setLoadedVideos(prev => prev + 1);
   }
 
-  // Path of the videos
+  const upComingVideos = (currentIndex % totalVideos) + 1;
+
+  const handleMiniVideoClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex(upComingVideos);    
+  }
+
   const getVideosSrc = (index) => `videos/hero-${index}.mp4`;
 
   return (
-    <div className='relative h-dvh w-screen overflow-x-hidden border-4'>
-        <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-bluey-75 border-4 border-red-400'>
+    <div className='relative h-dvh w-screen overflow-x-hidden'>
+        <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-bluey-75'>
           <div>
-            <div onClick={miniVideoClick} className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg border-4'>
-              <video 
-                ref={nextVideoRef} 
-                src={getVideosSrc(currentIndex + 1)}
+            <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
+              <div onClick={handleMiniVideoClick}  className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
+                <video
+                  ref={nextVideoRef}
+                  src={getVideosSrc(upComingVideos)}
+                  loop
+                  muted
+                  id='current-index'
+                  className='size-64 origin-center scale-150 object-cover object-center'
+                  onLoadedData={handleVideoLoad}
+                  >     
+                </video>
+              </div>
+            </div>
+
+            <video 
+                ref={nextVideoRef}
+                src={getVideosSrc(currentIndex)}
                 loop
                 muted
-                id='current-index'
-                className='size-64 origin-center scale-150 object-cover object-center border-red-700 border-4'>
-                
-              </video>
+                id='next-video'
+                className='absolute-center invisible absolute z-20 object-cover object-center size-64'
+                onLoadedData={handleVideoLoad}
+            >                
+            </video>     
+
+            <video 
+              src={getVideosSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+              autoPlay
+              loop 
+              muted
+              className='absolute top-0 left-0 size-full object-cover object-center'
+            ></video>
+
+            <h1 className='special-font hero-heading absolute z-40 bottom-5 right-5 text-bluey-75'>
+              G<b>a</b>ming
+            </h1>
+
+            <div className='absolute z-40 left-0 top-0 size-full'>
+              <div className='mt-24 px-5 sm:px-10 text-bluey-100'>
+                <h1 className='special-font hero-heading'>
+                  Redi<b>n</b>e
+                </h1>
+                <p className='mb-5 max-w-64 font-robert-regular'>
+                  Enter the Meta Game Layer <br />
+                  Unleash the Play Economy
+                </p>
+              </div>
             </div>
           </div>
         </div>
